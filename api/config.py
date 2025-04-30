7701defb8e7c582047e423a6d837a86cd9511df1
Shovick Barua <shovickbarua92@gmail.com>
2025-04-30 21:59:59 +0600

    cors access

--- api/config.py
+++ api/config.py
@@ -3,6 +3,7 @@ GMAIL_USER = "nebulacore.inc@gmail.com"
 GMAIL_APP_PASSWORD = "nmdxfqzhijohlews"
 
 ALLOWED_ORIGINS = [
+    "https://www.nebulacore.dev",
     "https://nebulacore.dev",
     "https://stagging.nebulacore.dev",
     "http://localhost:8080",
